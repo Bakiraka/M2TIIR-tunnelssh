8c8343a442f54b8229c90b2c093e6bb5e95124f1
Bakiraka <anthony.djenane@gmail.com>
2015-10-12 18:00:10 +0200

    début, création client et serveur

---
+++ client.py
@@ -0,0 +1,13 @@
+import urllib.request
+import urllib.error
+
+data = bytes("test data", 'UTF-8')
+req = urllib.request.Request('http://localhost:8888', data)
+req.add_header('Content-Length', '%d' % len(data))
+req.add_header('Content-Type', 'application/octet-stream')
+
+try:
+    res = urllib.request.urlopen(req)
+    print(res.read())
+except (urllib.error.HTTPError, urllib.error.URLError) as e:
+    print(e)
---
+++ server.py
@@ -0,0 +1,60 @@
+import sys
+import urllib.parse
+import logging, cgi
+import http.server, socketserver
+
+######### application/octet-stream
+
+
+class MethodHandler(http.server.SimpleHTTPRequestHandler):
+
+    def __init__(self,req,client_addr,server):
+        http.server.SimpleHTTPRequestHandler.__init__(self,req,client_addr,server)
+'''
+    def do_GET(self):
+        # Parse query data & params to find out what was passed
+        parsedParams = urllib.parse.urlparse(self.path)
+        queryParsed = urllib.parse.parse_qs(parsedParams.query)
+
+        # request is either for a file to be served up or our test
+        if parsedParams.path == "/ssh":
+            self.processMySSHRequest(queryParsed)
+        else:
+            # Default to serve up a local file
+            http.server.SimpleHTTPRequestHandler.do_GET(self);
+
+
+    def processMySSHRequest(self, query):
+        DUMMY_RESPONSE = "TEST"
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-length", len(DUMMY_RESPONSE))
+        self.end_headers()
+        self.wfile.write(bytes(DUMMY_RESPONSE, 'UTF-8'))
+        return
+'''
+
+    def do_POST(self):
+        # Parse the form data posted
+        form = cgi.parse_multipart()
+        print("##########################")
+        print(form)
+        print("##########################")
+
+        # Begin the response
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("Hello ! You did a POST !",'utf-8'))
+        return
+
+if __name__ == '__main__':
+    PORT = 8888
+    try:
+        httpd = socketserver.TCPServer(("", PORT), MethodHandler)
+    except OSError as bleme:
+        print("Error ! " + str(bleme))
+        sys.exit()
+
+    print("Starting server at port " + str(PORT) + ", use <Ctrl-C> to stop")
+    httpd.serve_forever()
