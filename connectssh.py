1129636399080f74c6dde13794e68e3ffacee9e2
blanquart julien <blanquart@pcmt10.m2fil.univ-lille1.fr>
2015-10-13 15:49:36 +0200

    modification machine boulot

--- connectssh.py
+++ connectssh.py
@@ -1,4 +1,4 @@
-PORT_SSHD = 4269
+PORT_SSHD = 22
 MAX_LENGTH = 4096
 closed = True
 ADDRESS_SSHD = "localhost"
@@ -10,32 +10,38 @@ OPEN_SOCKET_MESSAGE = 'TO_OPEN'
 ASK_COMMAND_MESSAGE = 'WAITING_FOR_COMMAND'
 EMPTY_MESSAGE = 'BLANK'
 import socket
+import sys
+import urllib.request
+import urllib.error
+import urllib.parse
 sock = socket.socket()
 content = ''
-data = ''
+toSend = bytes(ASK_COMMAND_MESSAGE, 'UTF-8')
+data = toSend
 while True :
-    data = bytes(data, 'UTF-8')
+    data = toSend
     req = urllib.request.Request('http://'+ADDRESS_SERVER+':'+PORT_SERVER, data)
     req.add_header('Content-Length', len(data))
     req.add_header('Content-Type', 'application/octet-stream')
-    res = urllib.request.urlopen(req)
-    content = res.read()
-    # si le message qu'on recoit demande de commencer la com ssh, on le fait, et on attend des commandes
-    if str(content) == OPEN_SOCKET_MESSAGE :
-        sock.connect(ADDRESS_SSHD, PORT_SSHD)
+    try :
+        res = urllib.request.urlopen(req)
+        content = res.read()
+    except (urllib.error.HTTPError, urllib.error.URLError) as e :
+        continue
+    if closed == True :
         closed = False
-        data = ASK_COMMAND_MESSAGE
+        sock.connect((ADDRESS_SSHD,PORT_SSHD))
+        sock.setblocking(False)
     # si le message demande qu'on close la socket, on le fait, et on redemande la connection au serveur
     if str(content) == CLOSE_SOCKET_MESSAGE :
         sock.close()
         closed = True
-        data = TRY_CONNECTION_MESSAGE
     # si le message n'est pas un message d'attente, on envoie la commande ssh, puis on recoit les données qu'on envoie
-    if closed == False and str(content) != EMPTY_MESSAGE :
-        socket.send(content)
-        toSend = socket.receive(MAX_LENGTH)
-        data = bytes(toSend, 'UTF-8')
-    # sinon on envoit une demande de commande
-    else :
-        data = ASK_COMMAND_MESSAGE
+    if str(content) != EMPTY_MESSAGE :
+        sock.send(content)
+    try :
+        toSend = sock.recv(MAX_LENGTH)
+        print(toSend)
+    except (BlockingIOError) as e :
+        toSend = bytes(ASK_COMMAND_MESSAGE, 'UTF-8')
     
