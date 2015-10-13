5c32b582685fcc9d49aa8456005f154c8a2e7232
root <root@pcmt10.m2fil.univ-lille1.fr>
2015-10-13 09:13:46 +0200

    premiere version du client

--- connectssh.py
+++ connectssh.py
@@ -1,16 +1,41 @@
-PORT = 4269
-SIZE = 256
-ADDRESS = "localhost"
+PORT_SSHD = 4269
+MAX_LENGTH = 4096
+closed = True
+ADDRESS_SSHD = "localhost"
+ADDRESS_SERVER = "localhost"
+PORT_SERVER = "8888"
+TRY_CONNECTION_MESSAGE = 'TRY_CONNECTION'
+CLOSE_SOCKET_MESSAGE = 'TO_CLOSE'
+OPEN_SOCKET_MESSAGE = 'TO_OPEN'
+ASK_COMMAND_MESSAGE = 'WAITING_FOR_COMMAND'
+EMPTY_MESSAGE = 'BLANK'
 import socket
 sock = socket.socket()
-sock.connect(ADDRESS, PORT)
+content = ''
+data = ''
 while True :
-    data = bytes("test data", 'UTF-8')
-    req = urllib.request.Request('http://localhost:8888', data)
-    req.add_header('Content-Length', '%d' % len(data))
+    data = bytes(data, 'UTF-8')
+    req = urllib.request.Request('http://'+ADDRESS_SERVER+':'+PORT_SERVER, data)
+    req.add_header('Content-Length', len(data))
     req.add_header('Content-Type', 'application/octet-stream')
     res = urllib.request.urlopen(req)
     content = res.read()
-    socket.send(content)
-    
+    # si le message qu'on recoit demande de commencer la com ssh, on le fait, et on attend des commandes
+    if str(content) == OPEN_SOCKET_MESSAGE :
+        sock.connect(ADDRESS_SSHD, PORT_SSHD)
+        closed = False
+        data = ASK_COMMAND_MESSAGE
+    # si le message demande qu'on close la socket, on le fait, et on redemande la connection au serveur
+    if str(content) == CLOSE_SOCKET_MESSAGE :
+        sock.close()
+        closed = True
+        data = TRY_CONNECTION_MESSAGE
+    # si le message n'est pas un message d'attente, on envoie la commande ssh, puis on recoit les données qu'on envoie
+    if closed == False and str(content) != EMPTY_MESSAGE :
+        socket.send(content)
+        toSend = socket.receive(MAX_LENGTH)
+        data = bytes(toSend, 'UTF-8')
+    # sinon on envoit une demande de commande
+    else :
+        data = ASK_COMMAND_MESSAGE
     
