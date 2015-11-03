140a6421601125400d3dcb0fcd2ad0cad7e9c0ea
Bakiraka <anthony.djenane@gmail.com>
2015-11-03 23:37:51 +0100

    tests de chiffrement

--- testcrypto.py
+++ testcrypto.py
@@ -1,18 +1,21 @@
 import binascii
+import sys
 
-password = '8a'
-password = bytes(binascii.a2b_hex(password))
+password = int(sys.argv[1],16)
+#password = bytes(binascii.a2b_hex(password))
 
 bytesarraytosend = bytearray(b'testchiffrement')
 
 print("A chiffrer : " + bytesarraytosend.decode())
 
 for i in range(len(bytesarraytosend)):
+    print("Element {} to cipher : {}".format(i, bytesarraytosend[i]))
     bytesarraytosend[i] ^= password
 
-print("Chiffré : " + bytesarraytosend.decode())
+print("Chiffré : " + str(bytesarraytosend))
 
 for i in range(len(bytesarraytosend)):
+    print("Element {} to decipher : {}".format(i, bytesarraytosend[i]))
     bytesarraytosend[i] ^= password
 
 print("Déchiffré : " + bytesarraytosend.decode())
